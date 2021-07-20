58a53e2a21063566061b3628b8d9b27a840a49b6
김예지 <yjiu9992@gmail.com>
2021-07-20 16:48:49 +0900

    시크릿키 수정

--- crud/settings.py
+++ crud/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-uxy=b^2rqtkd8hy&i_z))r*+_ay@(hiht))y4c8jlvaol3489s')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-uxy=b^2rqtkd8hy&i_z))r*+_ay@(hiht))y4c8jlvaol3489s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
